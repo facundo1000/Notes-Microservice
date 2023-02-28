84bbadb30cca9e51c54d95ed487e48b6b887696e
Mr.Fish <martinez.facundo85@gmail.com>
2023-02-27 21:02:53 -0300

    add: file with constant values

---
+++ src/main/java/org/fmartinez/api/note/service/constant/ApplicationConstant.java
@@ -0,0 +1,6 @@
+package org.fmartinez.api.note.service.constant;
+
+public class ApplicationConstant {
+    public static final String NOTE_PATH = "/api/mongo";
+    public static final String USER_PATH = "/api/user";
+}
