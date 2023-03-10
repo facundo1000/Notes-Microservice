5da5d2772a9ed1e97afb40150f73823d0f956eb1
Mr.Fish <martinez.facundo85@gmail.com>
2023-03-10 17:01:19 -0300

    add: constructor

--- src/main/java/org/fmartinez/api/note/service/dto/user/UserResponse.java
+++ src/main/java/org/fmartinez/api/note/service/dto/user/UserResponse.java
@@ -1,5 +1,6 @@
 package org.fmartinez.api.note.service.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,4 +13,12 @@ import java.util.List;
 @Data
 public class UserResponse extends UserRequest {
     private List<Note> note;
+
+    public UserResponse(@NotBlank(message = "username is required") String username,
+                        @NotBlank(message = "email is required") String email,
+                        @NotBlank(message = "password is required") String password,
+                        List<Note> note) {
+        super(username, email, password);
+        this.note = note;
+    }
 }
