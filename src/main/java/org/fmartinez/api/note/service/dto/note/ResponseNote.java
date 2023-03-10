12388a84cf9821e95241ea64c9dcc071d7796b1c
Mr.Fish <martinez.facundo85@gmail.com>
2023-03-10 16:59:47 -0300

    add: constructor

--- src/main/java/org/fmartinez/api/note/service/dto/note/ResponseNote.java
+++ src/main/java/org/fmartinez/api/note/service/dto/note/ResponseNote.java
@@ -1,7 +1,9 @@
 package org.fmartinez.api.note.service.dto.note;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,4 +18,14 @@ public class ResponseNote extends RequestNote {
     private LocalDateTime updated;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime created;
+
+    public ResponseNote(@NotBlank(message = "title must not be null")
+                        String title,
+                        @NotBlank(message = "note must not be null") String note,
+                        LocalDateTime updated,
+                        LocalDateTime created) {
+        super(title, note);
+        this.updated = updated;
+        this.created = created;
+    }
 }
