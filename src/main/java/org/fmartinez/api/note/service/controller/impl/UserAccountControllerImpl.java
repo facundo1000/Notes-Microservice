86c585110ee9b880fd0fd7f9803223c48d50a219
Mr.Fish <martinez.facundo85@gmail.com>
2023-03-10 17:48:40 -0300

    fix: validation implementation

--- src/main/java/org/fmartinez/api/note/service/controller/impl/UserAccountControllerImpl.java
+++ src/main/java/org/fmartinez/api/note/service/controller/impl/UserAccountControllerImpl.java
@@ -2,7 +2,6 @@ package org.fmartinez.api.note.service.controller.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.fmartinez.api.note.service.controller.UserAccountController;
-import org.fmartinez.api.note.service.dto.UserResponse;
 import org.fmartinez.api.note.service.entity.UserAccount;
 import org.fmartinez.api.note.service.service.UserAccountService;
 import org.springframework.http.HttpStatus;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,39 +31,55 @@ public class UserAccountControllerImpl implements UserAccountController {
     }
 
     @Override
+    public ResponseEntity<UserAccount> findUserByEmail(String email) {
+        return ResponseEntity.ok(service.findUserByEmail(email));
+    }
+
+    @Override
     public ResponseEntity<?> create(UserAccount user, BindingResult result) {
+        try {
+            Map<String, String> errors = new HashMap<>();
 
-        Map<String, String> errors = new HashMap<>();
+            if (result.hasErrors()) {
+                result.getFieldErrors()
+                        .forEach(field -> errors.put(field.getField(), "Field "
+                                .concat(field.getField())
+                                .concat(" ")
+                                .concat(Objects.requireNonNull(field.getDefaultMessage()))));
 
-        if (!result.hasErrors()) {
+                return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+            }
             return new ResponseEntity<>(service.create(user), HttpStatus.CREATED);
-        }
-        result.getFieldErrors()
-                .forEach(field -> errors.put("error", field.getDefaultMessage()));
 
-        return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("UserAccount creation error");
+        }
     }
 
     @Override
     public ResponseEntity<?> update(UserAccount user, BindingResult result, String id) {
-        Map<String, String> errors = new HashMap<>();
-        UserResponse response;
+        try {
+            if (result.hasErrors()) {
+                Map<String, String> errors = new HashMap<>();
+                result.getFieldErrors()
+                        .forEach(field -> errors.put(field.getField(), "Field "
+                                .concat(field.getField())
+                                .concat(" ")
+                                .concat(Objects.requireNonNull(field.getDefaultMessage()))));
 
-        if (!result.hasErrors()) {
-            service.update(user, id);
-            response = new UserResponse(user.getUsername(), user.getEmail());
-            return new ResponseEntity<>(response, HttpStatus.OK);
+                return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+            }
+
+            return new ResponseEntity<>(service.update(user, id), HttpStatus.OK);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("UserAccount update error");
         }
-        result.getFieldErrors()
-                .forEach(field -> errors.put("error", field.getDefaultMessage()));
-
-        return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
     }
 
     @Override
     public ResponseEntity<?> addNoteToUser(String idNote, String idUser) {
         UserAccount account = service.addNote(idNote, idUser);
-        return new ResponseEntity<>(account,HttpStatus.OK);
+        return new ResponseEntity<>(account, HttpStatus.OK);
     }
 
     @Override
