03f551a295f0ffc9e09e74b3202872f3253a7383
Mr.Fish <martinez.facundo85@gmail.com>
2023-03-10 16:58:59 -0300

    add: email regex constant

--- src/main/java/org/fmartinez/api/note/service/constant/ApplicationConstant.java
+++ src/main/java/org/fmartinez/api/note/service/constant/ApplicationConstant.java
@@ -3,4 +3,6 @@ package org.fmartinez.api.note.service.constant;
 public class ApplicationConstant {
     public static final String NOTE_PATH = "/api/mongo";
     public static final String USER_PATH = "/api/user";
+
+    public static final String EMAIL_REGEX = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
 }
